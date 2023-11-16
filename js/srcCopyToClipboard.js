79dbff575460c7b0c343ed9b4261fc94a6fac30e
roku-sioku <149998328+roku-sioku@users.noreply.github.com>
2023-11-16 17:07:31 +0900

    commit!

--- js/srcCopyToClipboard.js
+++ js/srcCopyToClipboard.js
@@ -2,7 +2,7 @@
 let img = document.getElementsByTagName('img');
 for (let i=0; i<img.length;i++){
     img[i].addEventListener('click',function(){
-        let src = img[i].getAttribute('src');
+        let src = img[i].getAttribute('https://roku-sioku.github.io/citrusix/'+'src');
         navigator.clipboard.writeText(src).then(
             () => {
                 alert("URLをコピーしました。");
