25508af359dd84e992abb589110f9fe4104d7bd1
roku-sioku <149998328+roku-sioku@users.noreply.github.com>
2024-10-22 16:27:43 +0900

    結果画像が表示されてないので調整

--- nantoka_maker/script.js
+++ nantoka_maker/script.js
@@ -248,7 +248,7 @@ function showResult(){
       if(document.getElementById("result-png")){
         document.getElementById("result-png").src = img_data;
       }else{
-        document.getElementsByClassName(".result-png")[0].appendChild(img_elem);
+        document.getElementById("result-png").appendChild(img_elem);
       }
 
           //表示と同時にダウンロードもさせる場合
